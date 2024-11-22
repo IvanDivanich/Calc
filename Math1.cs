f52dc577b4a1c3cf44f70a5f02310794a8b82b4d
Katya <k.borisova.652@mail.ru>
2024-11-22 13:07:52 +0300

    commit1

---
+++ Math1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    internal class Math1
+    { 
+        public double Sum (int a, int b)
+            { return a + b; }
+
+        public double Deference (int a, int b)
+            { return a - b; }
+    }
+}
