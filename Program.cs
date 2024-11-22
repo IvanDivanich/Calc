e6e9867bf45813a9be01c202b9011c1f8528fec5
Ivan <ivan-katin2004@yandex.ru>
2024-11-22 11:14:12 +0300

    end commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,37 @@ namespace Calc
     {
         static void Main(string[] args)
         {
+            int n, x, y;
+
+            SpecialFuns sp = new SpecialFuns();
+            Console.WriteLine("Введите номер операции\n1 - Плюс\n2 - Минус\n3 - Разделить\n4 - Умножить\n5 - Степень");
+
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите первое число");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            y = int.Parse(Console.ReadLine());
+
+            switch (n)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    Console.WriteLine(sp.Degree(x, y));
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ SpecialFuns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    internal class SpecialFuns
+    {
+        public double Degree(int x, int degree)
+        {
+            return Math.Pow(x, degree);
+        }
+
+        
+    }
+}
